513a0e323abc2772f6bc87a4209d679d6e4ba121
Tamilselvan K <tamilselvanks007@gamil.com>
2023-05-03 20:18:59 +0530

    routes🎉

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
-import {
-  createTenants,
-  readAllTenants,
-  readTenantById,
-  updateTenantById,
-  deleteTenantById,
-} from "./helper.js";
+import { tenantsRouter } from "./routes/tenants.js";
 
 dotenv.config();
 
@@ -31,14 +25,6 @@ async function createConnection() {
 }
 export const client = await createConnection();
 
-// Create Method (POST)
-// creting a server for post tenants
-app.post("/tenants", async function (req, res) {
-  const data = req.body;
-  const result = await createTenants(data);
-  res.send(result);
-});
-
 // Read Method (GET)
 // creating a server for home page
 app.get("/", function (req, res) {
@@ -47,36 +33,6 @@ app.get("/", function (req, res) {
   );
 });
 
-// creating a server for read all tenants
-app.get("/tenants", async function (req, res) {
-  const result = await readAllTenants();
-  res.send(result);
-});
-
-// creating a server fo asyncr read tenants by id
-app.get("/tenants/:id", async function (req, res) {
-  const { id } = req.params;
-  const result = await readTenantById(id);
-  result
-    ? res.send(result)
-    : res.status(404).send({ message: "No such tenant found" });
-});
-
-// Delete Method (DELETE)
-// creating a server for delete tenants by id
-app.delete("/tenants/:id", async function (req, res) {
-  const { id } = req.params;
-  const result = await deleteTenantById(id);
-  res.send(result);
-});
-
-//  Update Method (PUT)
-// creating a server for update tenents details by id
-app.put("/tenants/:id", async function (req, res) {
-  const { id } = req.params;
-  const updateDate = req.body;
-  const result = await updateTenantById(id, updateDate);
-  res.send(result);
-});
+app.use("/tenants", tenantsRouter);
 
 app.listen(PORT, () => console.log(`Server started in ${PORT}`));
---
+++ routes/tenants.js
@@ -0,0 +1,54 @@
+import express from "express";
+import {
+  createTenants,
+  readAllTenants,
+  readTenantById,
+  updateTenantById,
+  deleteTenantById,
+} from "../helper.js";
+
+const router = express.Router();
+
+// Create Method (POST)
+// creting a server for post tenants
+router.post("/", async function (req, res) {
+  const data = req.body;
+  const result = await createTenants(data);
+  res.send(result);
+});
+
+// Read Method (GET)
+// creating a server for read all tenants
+
+router.get("/", async function (req, res) {
+  const result = await readAllTenants();
+  res.send(result);
+});
+
+// creating a server for read tenants by id
+router.get("/:id", async function (req, res) {
+  const { id } = req.params;
+  const result = await readTenantById(id);
+  result
+    ? res.send(result)
+    : res.status(404).send({ message: "No such tenant found" });
+});
+
+//  Update Method (PUT)
+// creating a server for update tenents details by id
+router.put("/:id", async function (req, res) {
+  const { id } = req.params;
+  const updateDate = req.body;
+  const result = await updateTenantById(id, updateDate);
+  res.send(result);
+});
+
+// Delete Method (DELETE)
+// creating a server for delete tenants by id
+router.delete("/:id", async function (req, res) {
+  const { id } = req.params;
+  const result = await deleteTenantById(id);
+  res.send(result);
+});
+
+export const tenantsRouter = router;
